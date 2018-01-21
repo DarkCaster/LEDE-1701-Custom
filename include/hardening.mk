c72c5cc083abc58a78fdaee448a5c14337c95eda
DarkCaster <dark.caster@outlook.com>
2018-01-21 22:07:05 +0300

    fixup! Set GCC 7 as default: update include/hardening.mk

--- include/hardening.mk
+++ include/hardening.mk
@@ -48,6 +48,6 @@ ifdef CONFIG_PKG_RELRO_FULL
   endif
 endif
 ifdef CONFIG_PKG_LOOSEN_OPTS
-  TARGET_CFLAGS += -Wno-error=all -Wno-error=format-truncation -Wno-error=misleading-indentation -Wno-error=pointer-compare -Wno-error=misleading-indentation -Wno-narrowing -Wno-implicit-fallthrough
-  TARGET_CXXFLAGS += -Wno-error=all -Wno-error=format-truncation -Wno-error=misleading-indentation -Wno-error=pointer-compare -Wno-error=misleading-indentation -Wno-narrowing -Wno-implicit-fallthrough -fpermissive
+  TARGET_CFLAGS += -Wno-error=all -Wno-error=memset-elt-size -Wno-error=format-truncation -Wno-error=pointer-compare -Wno-error=misleading-indentation -Wno-narrowing -Wno-implicit-fallthrough
+  TARGET_CXXFLAGS += -Wno-error=all -Wno-error=memset-elt-size -Wno-error=format-truncation -Wno-error=pointer-compare -Wno-error=misleading-indentation -Wno-narrowing -Wno-implicit-fallthrough -fpermissive
 endif
